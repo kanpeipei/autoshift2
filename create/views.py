05ee6bc3da1e120c00cf2ea686eea6f50bc94bd4
kanpeipei <satosjhkkmp@gmail.com>
2019-12-11 21:08:53 +0900

    memberとshiftを入れ替え

--- create/views.py
+++ create/views.py
@@ -33,11 +33,17 @@ class CreateShiftViews(View):
 create = CreateShiftViews.as_view()
 
 
+# シフト自動作成
 def auto_create(year, month):
+    # 希望シフトを収集
     requested_shifts = RequestedShifts.objects.all().filter(year=year, month=month)
+    # 欠勤のシフトを除外
     candidate_shifts = [requested_shift for requested_shift in requested_shifts if not requested_shift.is_absence]
+    # random.choiceする配列
     choice_members = []
+    # 日にち分繰り返す
     for day in range(1, 16):
+        # 決定シフトを格納
         final_day_shifts = dict.fromkeys([
             'opener',
             'closer'
@@ -52,9 +58,11 @@ def auto_create(year, month):
             'kitchen_pm1',
             'kitchen_pm2',
         ])
+        # その日のシフトを収集
         day_shifts = [candidate_shift for candidate_shift in candidate_shifts
                       if candidate_shift.day == day
                       ]
+        # 各ポジションのシフトを収集
         opener_shifts = [day_shift for day_shift in day_shifts
                          if day_shift.member.is_opener and day_shift.since == 9]
         smg_shifts = [day_shift for day_shift in day_shifts
@@ -68,20 +76,15 @@ def auto_create(year, month):
         counter_am4_shifts = [day_shift for day_shift in day_shifts
                               if day_shift.member.is_counter and day_shift.since <= 12]
         counter_pm1_shifts = [day_shift for day_shift in day_shifts
-                              if day_shift.member.is_counter and day_shift.to == 22]
-        # counter_pm2_shifts = [day_shift for day_shift in day_shifts
-        #                       if day_shift.member.is_counter and day_shift.to == 22]
-        # counter_pm3_shifts = [day_shift for day_shift in day_shifts
-        #                       if day_shift.member.is_counter and day_shift.to == 22]
+                              if day_shift.member.is_counter and day_shift.since <= 17 and day_shift.to == 22]
         kitchen_am1_shifts = [day_shift for day_shift in day_shifts
                               if day_shift.member.is_kitchen and day_shift.since == 9 and day_shift.to >= 17]
         kitchen_pm1_shifts = [day_shift for day_shift in day_shifts
                               if day_shift.member.is_kitchen and day_shift.since <= 17 and day_shift.to == 22]
-        # kitchen_pm2_shifts = [day_shift for day_shift in day_shifts
-        #                       if day_shift.member.is_kitchen and day_shift.to == 22]
 
-        choice_members.clear()
         final_smg_shifts = FinalSmgShifts()
+        # オープンメンバーの決定
+        # クローズメンバーの候補を決定
         if opener_shifts:
             final_day_shifts['opener'] = random.choice(opener_shifts)
             if final_day_shifts['opener'].to >= 17:
@@ -99,6 +102,7 @@ def auto_create(year, month):
             final_personal_shifts_opener.save()
             final_smg_shifts.open_shifts = final_personal_shifts_opener
 
+            # シフトが決定したメンバーを各ポジションから除外
             if final_day_shifts['opener'] in counter_am1_shifts:
                 counter_am1_shifts.remove(final_day_shifts['opener'])
             if final_day_shifts['opener'] in counter_am2_shifts:
@@ -109,39 +113,24 @@ def auto_create(year, month):
                 counter_am4_shifts.remove(final_day_shifts['opener'])
             if final_day_shifts['opener'] in counter_pm1_shifts:
                 counter_pm1_shifts.remove(final_day_shifts['opener'])
-            # if final_day_shifts['opener'] in counter_pm2_shifts:
-            #     counter_pm2_shifts.remove(final_day_shifts['opener'])
-            # if final_day_shifts['opener'] in counter_pm3_shifts:
-            #     counter_pm3_shifts.remove(final_day_shifts['opener'])
             if final_day_shifts['opener'] in kitchen_am1_shifts:
                 kitchen_am1_shifts.remove(final_day_shifts['opener'])
             if final_day_shifts['opener'] in kitchen_pm1_shifts:
                 kitchen_pm1_shifts.remove(final_day_shifts['opener'])
-            # if final_day_shifts['opener'] in kitchen_pm2_shifts:
-            #     kitchen_pm2_shifts.remove(final_day_shifts['opener'])
 
             if final_day_shifts['opener'].to >= 17:
                 choice_members = [smg_shift for smg_shift in smg_shifts
                                   if smg_shift.since <= 17
                                   ]
-                # for smg_shift in smg_shifts:
-                #     if smg_shift.since <= 17:
-                #         choice_members.append(smg_shift)
             elif final_day_shifts['opener'].to < 17:
                 choice_members = [smg_shift for smg_shift in smg_shifts
                                   if smg_shift.since <= final_day_shifts['opener'].to
                                   ]
-                # for smg_shift in smg_shifts:
-                #     if smg_shift.since <= final_day_shifts['opener'].to:
-                #         choice_members.append(smg_shift)
         else:
-            # クローズメンバーの候補を選択
             choice_members = [smg_shift for smg_shift in smg_shifts
                               if smg_shift.since <= 17
                               ]
-            # for smg_shift in smg_shifts:
-            #     if smg_shift.since <= 17:
-            #         choice_members.append(smg_shift)
+        # クローズメンバーの決定
         if choice_members:
             final_day_shifts['closer'] = random.choice(choice_members)
             choice_members.clear()
@@ -167,7 +156,7 @@ def auto_create(year, month):
 
             final_personal_shifts_closer.save()
             final_smg_shifts.close_shifts = final_personal_shifts_closer
-
+            # シフトが決定したメンバーを各ポジションから除外
             if final_day_shifts['closer'] in counter_am1_shifts:
                 counter_am1_shifts.remove(final_day_shifts['closer'])
             if final_day_shifts['closer'] in counter_am2_shifts:
@@ -178,23 +167,17 @@ def auto_create(year, month):
                 counter_am4_shifts.remove(final_day_shifts['closer'])
             if final_day_shifts['closer'] in counter_pm1_shifts:
                 counter_pm1_shifts.remove(final_day_shifts['closer'])
-            # if final_day_shifts['closer'] in counter_pm2_shifts:
-            #     counter_pm2_shifts.remove(final_day_shifts['closer'])
-            # if final_day_shifts['closer'] in counter_pm3_shifts:
-            #     counter_pm3_shifts.remove(final_day_shifts['closer'])
             if final_day_shifts['closer'] in kitchen_am1_shifts:
                 kitchen_am1_shifts.remove(final_day_shifts['closer'])
             if final_day_shifts['closer'] in kitchen_pm1_shifts:
                 kitchen_pm1_shifts.remove(final_day_shifts['closer'])
-            # if final_day_shifts['closer'] in kitchen_pm2_shifts:
-            #     kitchen_pm2_shifts.remove(final_day_shifts['closer'])
 
         final_smg_shifts.save()
 
         final_counter_shifts = FinalCounterShifts()
         counter_am = []
+        # カウンター午前メンバーを決定
         if counter_am1_shifts:
-            # カウンター午前メンバーを決定
             final_day_shifts['counter_am1'] = random.choice(counter_am1_shifts)
             if final_day_shifts['counter_am1'].to < 17:
                 counter_am.append(final_day_shifts['counter_am1'].to)
@@ -211,7 +194,7 @@ def auto_create(year, month):
                 )
             final_personal_shifts_counter_am1.save()
             final_counter_shifts.am1_shifts = final_personal_shifts_counter_am1
-            # シフトが決定したパートナーを各ポジションから除外
+            # シフトが決定したメンバーを各ポジションから除外
             if final_day_shifts['counter_am1'] in counter_am2_shifts:
                 counter_am2_shifts.remove(final_day_shifts['counter_am1'])
             if final_day_shifts['counter_am1'] in counter_am3_shifts:
@@ -222,10 +205,7 @@ def auto_create(year, month):
                 kitchen_am1_shifts.remove(final_day_shifts['counter_am1'])
             if final_day_shifts['counter_am1'] in kitchen_pm1_shifts:
                 kitchen_pm1_shifts.remove(final_day_shifts['counter_am1'])
-            # if final_day_shifts['counter_am1'] in kitchen_pm2_shifts:
-            #     kitchen_pm2_shifts.remove(final_day_shifts['counter_am1'])
         if counter_am2_shifts:
-            # カウンター午前メンバーを決定
             final_day_shifts['counter_am2'] = random.choice(counter_am2_shifts)
             if final_day_shifts['counter_am2'].to < 17:
                 counter_am.append(final_day_shifts['counter_am2'].to)
@@ -242,7 +222,7 @@ def auto_create(year, month):
                 )
             final_personal_shifts_counter_am2.save()
             final_counter_shifts.am2_shifts = final_personal_shifts_counter_am2
-            # シフトが決定したパートナーを各ポジションから除外
+            # シフトが決定したメンバーを各ポジションから除外
             if final_day_shifts['counter_am2'] in counter_am3_shifts:
                 counter_am3_shifts.remove(final_day_shifts['counter_am2'])
             if final_day_shifts['counter_am2'] in counter_am4_shifts:
@@ -251,10 +231,7 @@ def auto_create(year, month):
                 kitchen_am1_shifts.remove(final_day_shifts['counter_am2'])
             if final_day_shifts['counter_am2'] in kitchen_pm1_shifts:
                 kitchen_pm1_shifts.remove(final_day_shifts['counter_am2'])
-            # if final_day_shifts['counter_am2'] in kitchen_pm2_shifts:
-            #     kitchen_pm2_shifts.remove(final_day_shifts['counter_am2'])
         if counter_am3_shifts:
-            # カウンター午前メンバーを決定
             final_day_shifts['counter_am3'] = random.choice(counter_am3_shifts)
             if final_day_shifts['counter_am3'].to < 17:
                 counter_am.append(final_day_shifts['counter_am3'].to)
@@ -271,17 +248,14 @@ def auto_create(year, month):
                 )
             final_personal_shifts_counter_am3.save()
             final_counter_shifts.am3_shifts = final_personal_shifts_counter_am3
-            # シフトが決定したパートナーを各ポジションから除外
+            # シフトが決定したメンバーを各ポジションから除外
             if final_day_shifts['counter_am3'] in counter_am4_shifts:
                 counter_am4_shifts.remove(final_day_shifts['counter_am3'])
             if final_day_shifts['counter_am3'] in kitchen_am1_shifts:
                 kitchen_am1_shifts.remove(final_day_shifts['counter_am3'])
             if final_day_shifts['counter_am3'] in kitchen_pm1_shifts:
                 kitchen_pm1_shifts.remove(final_day_shifts['counter_am3'])
-            # if final_day_shifts['counter_am3'] in kitchen_pm2_shifts:
-            #     kitchen_pm2_shifts.remove(final_day_shifts['counter_am3'])
         if counter_am4_shifts:
-            # カウンター午前メンバーを決定
             final_day_shifts['counter_am4'] = random.choice(counter_am4_shifts)
             if final_day_shifts['counter_am4'].to < 17:
                 counter_am.append(final_day_shifts['counter_am4'].to)
@@ -298,23 +272,18 @@ def auto_create(year, month):
                 )
             final_personal_shifts_counter_am4.save()
             final_counter_shifts.am4_shifts = final_personal_shifts_counter_am4
-            # シフトが決定したパートナーを各ポジションから除外
+            # シフトが決定したメンバーを各ポジションから除外
             if final_day_shifts['counter_am4'] in kitchen_am1_shifts:
                 kitchen_am1_shifts.remove(final_day_shifts['counter_am4'])
             if final_day_shifts['counter_am4'] in kitchen_pm1_shifts:
                 kitchen_pm1_shifts.remove(final_day_shifts['counter_am4'])
-            # if final_day_shifts['counter_am4'] in kitchen_pm2_shifts:
-            #     kitchen_pm2_shifts.remove(final_day_shifts['counter_am4'])
 
+        # カウンター午後メンバーを決定
         if counter_pm1_shifts:
             if counter_am:
-                # list.sort(counter_am, reverse=True)
                 loop = len(counter_am) - 2
                 counter_am.sort()
                 counter_am = counter_am[0:loop]
-                # final_personal_shifts_pm1 = None
-                # final_personal_shifts_pm2 = None
-                # final_personal_shifts_pm3 = None
                 final_day_shifts['counter_pm1'] = None
                 final_day_shifts['counter_pm2'] = None
                 final_day_shifts['counter_pm3'] = None
@@ -331,6 +300,7 @@ def auto_create(year, month):
                         )
                         final_personal_shifts_counter_pm1.save()
                         final_counter_shifts.pm1_shifts = final_personal_shifts_counter_pm1
+                        # シフトが決定したメンバーを各ポジションから除外
                         if final_day_shifts['counter_pm1'] in counter_pm1_shifts:
                             counter_pm1_shifts.remove(final_day_shifts['counter_pm1'])
                         if final_day_shifts['counter_pm1'] in kitchen_am1_shifts:
@@ -346,6 +316,7 @@ def auto_create(year, month):
                         )
                         final_personal_shifts_counter_pm2.save()
                         final_counter_shifts.pm2_shifts = final_personal_shifts_counter_pm2
+                        # シフトが決定したメンバーを各ポジションから除外
                         if final_day_shifts['counter_pm2'] in counter_pm1_shifts:
                             counter_pm1_shifts.remove(final_day_shifts['counter_pm2'])
                         if final_day_shifts['counter_pm2'] in kitchen_am1_shifts:
@@ -362,6 +333,7 @@ def auto_create(year, month):
                         )
                         final_personal_shifts_counter_pm3.save()
                         final_counter_shifts.pm3_shifts = final_personal_shifts_counter_pm3
+                        # シフトが決定したメンバーを各ポジションから除外
                         if final_day_shifts['counter_pm3'] in counter_pm1_shifts:
                             counter_pm1_shifts.remove(final_day_shifts['counter_pm3'])
                         if final_day_shifts['counter_pm3'] in kitchen_am1_shifts:
@@ -369,7 +341,7 @@ def auto_create(year, month):
                         if final_day_shifts['counter_pm3'] in kitchen_pm1_shifts:
                             kitchen_pm1_shifts.remove(final_day_shifts['counter_pm3'])
 
-                counter_am.clear()
+                    counter_am.clear()
                 if final_day_shifts['counter_pm2'] is None and counter_pm1_shifts:
                     final_day_shifts['counter_pm2'] = random.choice(counter_pm1_shifts)
                     final_personal_shifts_counter_pm2 = FinalPersonalShifts(
@@ -379,6 +351,7 @@ def auto_create(year, month):
                     )
                     final_personal_shifts_counter_pm2.save()
                     final_counter_shifts.pm2_shifts = final_personal_shifts_counter_pm2
+                    # シフトが決定したメンバーを各ポジションから除外
                     if final_day_shifts['counter_pm2'] in counter_pm1_shifts:
                         counter_pm1_shifts.remove(final_day_shifts['counter_pm2'])
                     if final_day_shifts['counter_pm2'] in kitchen_am1_shifts:
@@ -395,6 +368,7 @@ def auto_create(year, month):
                     )
                     final_personal_shifts_counter_pm3.save()
                     final_counter_shifts.pm3_shifts = final_personal_shifts_counter_pm3
+                    # シフトが決定したメンバーを各ポジションから除外
                     if final_day_shifts['counter_pm3'] in counter_pm1_shifts:
                         counter_pm1_shifts.remove(final_day_shifts['counter_pm3'])
                     if final_day_shifts['counter_pm3'] in kitchen_am1_shifts:
@@ -411,6 +385,7 @@ def auto_create(year, month):
                     )
                     final_personal_shifts_counter_pm1.save()
                     final_counter_shifts.pm1_shifts = final_personal_shifts_counter_pm1
+                    # シフトが決定したメンバーを各ポジションから除外
                     if final_day_shifts['counter_pm1'] in counter_pm1_shifts:
                         counter_pm1_shifts.remove(final_day_shifts['counter_pm1'])
                     if final_day_shifts['counter_pm1'] in kitchen_am1_shifts:
@@ -427,6 +402,7 @@ def auto_create(year, month):
                     )
                     final_personal_shifts_counter_pm2.save()
                     final_counter_shifts.pm2_shifts = final_personal_shifts_counter_pm2
+                    # シフトが決定したメンバーを各ポジションから除外
                     if final_day_shifts['counter_pm2'] in counter_pm1_shifts:
                         counter_pm1_shifts.remove(final_day_shifts['counter_pm2'])
                     if final_day_shifts['counter_pm2'] in kitchen_am1_shifts:
@@ -443,6 +419,7 @@ def auto_create(year, month):
                     )
                     final_personal_shifts_counter_pm3.save()
                     final_counter_shifts.pm3_shifts = final_personal_shifts_counter_pm3
+                    # シフトが決定したメンバーを各ポジションから除外
                     if final_day_shifts['counter_pm3'] in counter_pm1_shifts:
                         counter_pm1_shifts.remove(final_day_shifts['counter_pm3'])
                     if final_day_shifts['counter_pm3'] in kitchen_am1_shifts:
@@ -452,6 +429,7 @@ def auto_create(year, month):
         final_counter_shifts.save()
 
         final_kitchen_shifts = FinalKitchenShifts()
+        # キッチン午前メンバーを決定
         if kitchen_am1_shifts and kitchen_am1_shifts:
             final_day_shifts['kitchen_am1'] = random.choice(kitchen_am1_shifts)
             final_personal_shifts_kitchen_am1 = FinalPersonalShifts(
@@ -461,7 +439,8 @@ def auto_create(year, month):
             )
             final_personal_shifts_kitchen_am1.save()
             final_kitchen_shifts.am1_shifts = final_personal_shifts_kitchen_am1
-        if kitchen_pm1_shifts and kitchen_pm1_shifts:
+        # キッチン午後メンバー
+        if kitchen_pm1_shifts:
             final_day_shifts['kitchen_pm1'] = random.choice(kitchen_pm1_shifts)
             final_personal_shifts_kitchen_pm1 = FinalPersonalShifts(
                 member=final_day_shifts['kitchen_pm1'].member,
@@ -470,9 +449,10 @@ def auto_create(year, month):
             )
             final_personal_shifts_kitchen_pm1.save()
             final_kitchen_shifts.pm1_shifts = final_personal_shifts_kitchen_pm1
+            # シフトが決定したメンバーを各ポジションから除外
             if final_day_shifts['kitchen_pm1'] in kitchen_pm1_shifts:
                 kitchen_pm1_shifts.remove(final_day_shifts['kitchen_pm1'])
-        if kitchen_pm1_shifts and kitchen_pm1_shifts:
+        if kitchen_pm1_shifts:
             final_day_shifts['kitchen_pm2'] = random.choice(kitchen_pm1_shifts)
             final_personal_shifts_kitchen_pm2 = FinalPersonalShifts(
                 member=final_day_shifts['kitchen_pm2'].member,
@@ -484,6 +464,7 @@ def auto_create(year, month):
 
         final_kitchen_shifts.save()
 
+        # 確定シフトをデータに保存（再作成）
         if FinalAllShifts.objects.filter(year=year, month=month, day=day).exists():
             final_all_shifts = FinalAllShifts.objects.all().get(
                 year=year,
@@ -493,6 +474,7 @@ def auto_create(year, month):
             final_all_shifts.smg_shifts = final_smg_shifts
             final_all_shifts.counter_shifts = final_counter_shifts
             final_all_shifts.kitchen_shifts = final_kitchen_shifts
+        # 確定シフトをデータに保存（初作成）
         else:
             final_all_shifts = FinalAllShifts(
                 year=year,
